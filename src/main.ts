c88256f033a9b186efd991e48f4bbc2e354d637f
Michaël André <michael.andre@usonhb.fr>
2024-02-08 18:33:29 +0100

    Refactor and add state reporting in tray

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,15 @@
-import { Menu, Tray, app } from "electron";
+import { app } from "electron";
 import log from "electron-log/main";
-import * as path from "path";
 import { Subscription } from "rxjs";
-import { observeScoreSheetUpdates } from "./fdme-provider";
+import { ScoreSheetSource } from "./score-sheet-provider";
 import { LiveSyncConsumer } from "./live-sync-consumer";
 import { LocalBroadcastConsumer } from "./local-broadcast";
-import { observeFdmeActiveMatchCode } from "./match-code";
-import { observeScorepadUpdates } from "./scorepad-provider";
+import { configureTrayIcon } from "./menu";
+import { ScorepadSource } from "./scorepad-provider";
 
-export type AppCheckTokenRequest = Readonly<{ appId: string; }>
-export type AppCheckToken = Readonly<{ token: string, expiresInMillis: number }>;
+export type SourceState = "off" | "ready" | "connected" | "error";
 
-let appTray: Tray | undefined;
+let trayCallback: (() => void) | undefined;
 let updatesSub: Subscription | undefined;
 
 log.initialize({ preload: true });
@@ -22,33 +20,26 @@ app.whenReady().then(() => {
   try {
     const testMode = app.commandLine.getSwitchValue("mode") == "test";
     if (testMode) console.debug("Running in test mode");
-  
+
+    const scoreSheetSoure = new ScoreSheetSource(testMode);
+    const scorepadSource = new ScorepadSource();
+
     // Configure tray icon
-    appTray = new Tray(path.join(__dirname, "..", "assets", "icon.png"));
-    const contextMenu = Menu.buildFromTemplate([
-      { label: "Fermer", type: "normal", click: () => app.quit() }
-    ]);
-    appTray.setToolTip("Synchronisation FDME");
-    appTray.setContextMenu(contextMenu);
-  
-    // Process updates
-    const fdmeUpdates = observeScoreSheetUpdates(testMode);
-    const scorepadUpdates = observeScorepadUpdates();
-    const matchCode = observeFdmeActiveMatchCode(testMode);
-  
+    trayCallback = configureTrayIcon(scoreSheetSoure.state, scorepadSource.state);
+
     updatesSub = new Subscription();
-    updatesSub.add(new LiveSyncConsumer().subscribe(fdmeUpdates, scorepadUpdates, matchCode));
-    updatesSub.add(new LocalBroadcastConsumer("192.168.0.200").subscribe(scorepadUpdates));  
+    updatesSub.add(new LiveSyncConsumer().subscribe(scoreSheetSoure.updates, scorepadSource.updates));
+    updatesSub.add(new LocalBroadcastConsumer("192.168.0.200").subscribe(scorepadSource.updates));
   } catch (error) {
     console.error("Initialization error");
     console.error(error);
   }
-  
+
 });
 
 app.on("before-quit", function () {
   updatesSub?.unsubscribe();
-  appTray?.destroy();
+  trayCallback?.();
 });
 
 app.on("window-all-closed", () => {
@@ -56,6 +47,7 @@ app.on("window-all-closed", () => {
 });
 
 export type MatchUpdate = Partial<{
+  matchCode: string,
   homeScore: number,
   awayScore: number,
   chrono: number,
--- src/scorepad-provider.ts
+++ src/scorepad-provider.ts
@@ -1,24 +1,43 @@
 import { entries, isEqual, merge } from "lodash";
 import * as net from "net";
 import * as os from "os";
-import { Observable, distinctUntilChanged, map, mergeMap, retry, scan, share, tap, timer } from "rxjs";
-import { MatchUpdate } from "./main";
+import { BehaviorSubject, EMPTY, Observable, defer, distinctUntilChanged, map, mergeMap, repeat, retry, scan, share, switchMap, tap, timer } from "rxjs";
+import { MatchUpdate, SourceState } from "./main";
 
-const scorepadServerPort = 4001;
-const serverRetryDelayMs = 10000;
-const errorRetryDelayMs = 5000;
+export class ScorepadSource {
 
-/**
- * Start a local TCP server for Scorepad connection and parse received data.
- * @param host Local IP host to listen to
- * @returns A shared stream of updates
- */
-export function observeScorepadUpdates(): Observable<MatchUpdate> {
-  return startScorepadServer().pipe(
-    map(frame => processScorepadMessage(frame) ?? {}),
+  private readonly scorepadServerPort = 4001;
+  private readonly scanInterfacesDelayMs = 20000;
+  private readonly serverRetryDelayMs = 10000;
+  private readonly processingErrorRetryDelayMs = 5000;
+
+  readonly state = new BehaviorSubject<SourceState>("off");
+
+  readonly updates: Observable<MatchUpdate> = defer(() => this.findLocalEthernetInterfaceHost()).pipe(
+    switchMap(host => {
+      if (host) {
+        return this.createTcpServer(host);
+      } else {
+        this.state.next("off");
+        return EMPTY;
+      }
+    }),
+    repeat({ delay: this.scanInterfacesDelayMs }),
+    retry({
+      delay: (e) => {
+        console.error(`Scorepad server error: ${e}`);
+        this.state.next("error");
+        return timer(this.serverRetryDelayMs);
+      }
+    }),
+    mergeMap(connection => {
+      return this.observeDataFrame(connection).pipe(
+        map(frame => this.processScorepadMessage(frame) ?? {} as MatchUpdate)
+      )
+    }),
     // Accumulate messages
     scan((state, update) => merge({}, state, update), {} as MatchUpdate),
-    distinctUntilChanged(isEqual),
+    distinctUntilChanged<MatchUpdate>(isEqual),
     tap(u => {
       console.debug(`Scorepad update: ${JSON.stringify(u)}`);
     }),
@@ -26,55 +45,51 @@ export function observeScorepadUpdates(): Observable<MatchUpdate> {
     retry({
       delay: (e) => {
         console.error(`Scorepad updates error: ${e}`);
-        return timer(errorRetryDelayMs);
+        this.state.next("error");
+        return timer(this.processingErrorRetryDelayMs);
       }
     }),
     share()
-  )
-}
+  );
 
-function findLocalEthernetHost(): string | null {
-  return entries(os.networkInterfaces())
-    .find(([k]) => k.startsWith("eth"))
-    ?.[1]
-    ?.find(i => i.family == "IPv4")
-    ?.address
-    ?? null;
-}
+  private async findLocalEthernetInterfaceHost(): Promise<string | null> {
+    return entries(os.networkInterfaces())
+      .find(([k]) => k.startsWith("eth"))
+      ?.[1]
+      ?.find(i => i.family == "IPv4")
+      ?.address
+      ?? null;
+  }
 
-function startScorepadServer(): Observable<Buffer> {
-  return new Observable<net.Socket>(sub => {
-    const host = findLocalEthernetHost();
-    if (!host) throw Error("Local wired network interface not found");
-    const server = net.createServer();
-    server.on("connection", (socket) => {
-      console.log(`Scorepad client connected from ${socket.remoteAddress}:${socket.remotePort}`);
-      sub.next(socket);
+  private createTcpServer(host: string): Observable<net.Socket> {
+    return new Observable<net.Socket>(sub => {
+      const server = net.createServer();
+      server.on("connection", (socket) => {
+        this.state.next("connected");
+        console.log(`Scorepad client connected from ${socket.remoteAddress}:${socket.remotePort}`);
+        sub.next(socket);
+      });
+      server.on("error", e => {
+        console.warn(`Scorepad server error: ${e}`);
+        sub.error(e);
+      });
+      server.listen(this.scorepadServerPort, host, () => {
+        this.state.next("ready");
+        console.log(`Scorepad server started on ${host}:${this.scorepadServerPort}`);
+      });
+      return () => {
+        try {
+          server.close(() => console.debug("Scorepad server closed"));
+          server.unref();
+        } catch (e) {
+          console.error(`Failed to close server: ${e}`);
+        }
+      };
     });
-    server.on("error", e => {
-      console.warn(`Scorepad server error: ${e}`);
-      sub.error(e);
-    });
-    server.listen(scorepadServerPort, host, () => {
-      console.log(`Scorepad server started on ${host}:${scorepadServerPort}`);
-    });
-    return () => {
-      try {
-        server.close(() => console.debug("Scorepad server closed"));
-        server.unref();
-      } catch (e) {
-        console.error(`Failed to close server: ${e}`);
-      }
-    };
-  }).pipe(
-    // Auto-retry on error
-    retry({
-      delay: (e) => {
-        console.error(`Scorepad server error: ${e}`);
-        return timer(serverRetryDelayMs);
-      }
-    }),
-    mergeMap(connection => new Observable<Buffer>(sub => {
+  }
+
+  private observeDataFrame(connection: net.Socket): Observable<Buffer> {
+    return new Observable<Buffer>(sub => {
       connection.on("data", (frame) => sub.next(frame));
       connection.on("close", () => sub.complete());
       connection.on("error", e => {
@@ -89,69 +104,62 @@ function startScorepadServer(): Observable<Buffer> {
           console.error(`Failed to close connection: ${e}`);
         }
       };
-    }).pipe(
-      // Auto-retry on error
-      retry({
-        delay: (e) => {
-          console.error(`Scorepad connection error: ${e}`);
-          return timer(serverRetryDelayMs);
-        }
-      })
-    ))
-  );
-}
-
-/**
- * Parse a data frame to extract chrono and score information.
- * https://static.bodet-sport.com/images/stories/EN/support/Pdfs/manuals/Scorepad/608264-Network%20output%20and%20protocols-Scorepad.pdf
- * @param frame A raw frame for Scorepad connection
- * @returns Parsed MatchUpdate with chrono and score
- */
-function processScorepadMessage(frame: Buffer): MatchUpdate | null {
-  // Omit frame prefix (SOH / Address / STX / CTRL) and suffix (ETX / LRC)
-  const msg = frame.subarray(4, -2);
-  if (msg[0] == 0x30 && msg[1] == 0x31 && msg[3] == 0x34) {
-    // Handball / Message 01: game clock
-
-    const clockStatus = msg[2];
-    // Type of clock from bit 0 of clock status byte
-    const isGameClock = ((clockStatus >>> 0) & 0x01) == 0;
-    if (!isGameClock) return null;
-    // Game clock status from bit 1 of clock status byte
-    const chronoStarted = ((clockStatus >>> 1) & 0x01) == 0;
-
-    // Chrono
-    const min = Number(String.fromCharCode(msg[4])) * 10 + Number(String.fromCharCode(msg[5]));
-    const sec = Number(String.fromCharCode(msg[6])) * 10 + Number(String.fromCharCode(msg[7]));
-    const chrono = min * 60 + sec;
-    const period = Number(String.fromCharCode(msg[10]));
-    if (isNaN(chrono)) {
-      console.warn("Invalid Scorepad frame for chrono");
-      return null;
-    }
-
-    return { chrono, chronoStarted, period: isNaN(period) ? undefined : period };
-
-  } else if (msg[0] == 0x30 && msg[1] == 0x32 && msg[2] == 0x34) {
-    // Handball / Message 02: scores
-
-    const homeScore = Number((
-      String.fromCharCode(msg[3])
-      + String.fromCharCode(msg[4])
-      + String.fromCharCode(msg[5])
-    ).trim());
-    const awayScore = Number((
-      String.fromCharCode(msg[6])
-      + String.fromCharCode(msg[7])
-      + String.fromCharCode(msg[8])
-    ).trim());
-    if (isNaN(homeScore) || isNaN(awayScore)) {
-      console.warn("Invalid Scorepad frame for score");
-      return null;
-    }
-
-    return { homeScore, awayScore };
-
+    });
   }
-  return null;
+
+  /**
+   * Parse a data frame to extract chrono and score information.
+   * https://static.bodet-sport.com/images/stories/EN/support/Pdfs/manuals/Scorepad/608264-Network%20output%20and%20protocols-Scorepad.pdf
+   * @param frame A raw frame for Scorepad connection
+   * @returns Parsed MatchUpdate with chrono and score
+   */
+  private processScorepadMessage(frame: Buffer): MatchUpdate | null {
+    // Omit frame prefix (SOH / Address / STX / CTRL) and suffix (ETX / LRC)
+    const msg = frame.subarray(4, -2);
+    if (msg[0] == 0x30 && msg[1] == 0x31 && msg[3] == 0x34) {
+      // Handball / Message 01: game clock
+
+      const clockStatus = msg[2];
+      // Type of clock from bit 0 of clock status byte
+      const isGameClock = ((clockStatus >>> 0) & 0x01) == 0;
+      if (!isGameClock) return null;
+      // Game clock status from bit 1 of clock status byte
+      const chronoStarted = ((clockStatus >>> 1) & 0x01) == 0;
+
+      // Chrono
+      const min = Number(String.fromCharCode(msg[4])) * 10 + Number(String.fromCharCode(msg[5]));
+      const sec = Number(String.fromCharCode(msg[6])) * 10 + Number(String.fromCharCode(msg[7]));
+      const chrono = min * 60 + sec;
+      const period = Number(String.fromCharCode(msg[10]));
+      if (isNaN(chrono)) {
+        console.warn("Invalid Scorepad frame for chrono");
+        return null;
+      }
+
+      return { chrono, chronoStarted, period: isNaN(period) ? undefined : period };
+
+    } else if (msg[0] == 0x30 && msg[1] == 0x32 && msg[2] == 0x34) {
+      // Handball / Message 02: scores
+
+      const homeScore = Number((
+        String.fromCharCode(msg[3])
+        + String.fromCharCode(msg[4])
+        + String.fromCharCode(msg[5])
+      ).trim());
+      const awayScore = Number((
+        String.fromCharCode(msg[6])
+        + String.fromCharCode(msg[7])
+        + String.fromCharCode(msg[8])
+      ).trim());
+      if (isNaN(homeScore) || isNaN(awayScore)) {
+        console.warn("Invalid Scorepad frame for score");
+        return null;
+      }
+
+      return { homeScore, awayScore };
+
+    }
+    return null;
+  }
+
 }
